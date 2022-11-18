d59f6117b107483a7df9c26291acab8487bac575
terenzzzz <terence592592@gmail.com>
2022-11-18 00:44:03 +0000

    'sync'

--- labSheet/week7/lab7.py
+++ labSheet/week7/lab7.py
@@ -39,22 +39,109 @@ filename = args[0]
 
 tweets = []
 
-with open(filename, 'r') as in_file:
+with open(filename, 'r', encoding='UTF-8') as in_file:
     reader = csv.reader(in_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
     for row in reader:
         tweets.append(row[2]) 
 
 print(f"Loaded {len(tweets)} tweets.")
+################################################################
 
 test_tweets = [
-    "That U.S.A. poster-print costs $12.40...",
+    "That U.S.A. poster-print costs $12.40... :)",
     "He says I'm depressed most of the time. #sad ",
     "For the first time I get to see @username actually being hateful! it was beautiful:)",
     '''"The San Francisco-based restaurant" they said, "doesn't charge $10.5"'''
 ]
 
+# for t in test_tweets:
+#     list = re.findall(r'''(?x)(?:[A-Z]\.)+ | \w+(?:-\w+)* | \$?\d+(?:\.\d+)?%? | \.\.\. |  [][.,;"'?():_`-]''',t)
+#     print (list)
+    
 ################################################################
-for t in test_tweets:
-    list = re.findall(r'(?x)(?:[A-Z]\.)+| \w+(?:-\w+)*| \$?\d+(?:\.\d+)?%?｜\.\.\.',t)
-    print (list)
+# TTR: Number of Types / Number of Words
+
+def calcTTR(tweet):
+    dict = {}
+    numOfWords = 0
+    for s in tweet:
+        for w in s:
+            if w in dict:
+                dict[w]+=1
+            else:
+                dict[w]=1
+            numOfWords += 1
+    
+    TTR = len(dict)/numOfWords
+    print("TTR: ",TTR)
+
+# Without preprocess
+nothing = []
+for t in tweets:
+    list = t.split(" ")
+    nothing.append(list)
+calcTTR(nothing)
+
+# Tokenised
+tokenised = []
+for t in tweets:
+    list = re.findall(r'''(?x)(?:[A-Z]\.)+ | \w+(?:-\w+)* | \$?\d+(?:\.\d+)?%? | \.\.\. |  [][.,;"'?():_`-]''',t)
+    tokenised.append(list)
+calcTTR(tokenised)
+
+# Tokenisation + lowercase
+tokenised_lower = []
+for t in tweets:
+    list = []
+    res = re.findall(r'''(?x)(?:[A-Z]\.)+ | \w+(?:-\w+)* | \$?\d+(?:\.\d+)?%? | \.\.\. |  [][.,;"'?():_`-]''',t)
+    for x in res:
+        list.append(x.lower())
+    tokenised_lower.append(list)               
+calcTTR(tokenised_lower)
+
+# Tokenisation + lowercase + tweets preprocessing
+tokenised_lower_pre = []
+for t in tweets:
+    list = []
+    res = re.sub(r"@\w+", "<MENTIONS>", t)
+    res = re.sub(r"#\w+", "<HASHTAGS>", res)
+    res = re.findall(r'''(?x)(?:[A-Z]\.)+ | \w+(?:-\w+)* | \$?\d+(?:\.\d+)?%? | \.\.\. |  [][.,;"'?():_`-]''',res)
+    # res_emoticon = re.sub(r"","EMOTICON",res_hashtags)
+    # res_emojis = re.sub(r"","EMOJIS",res_hashtags)
+    for x in res:
+        list.append(x.lower())
+    tokenised_lower_pre.append(list)               
+calcTTR(tokenised_lower_pre)
+
+
+
+
+
+# for t in tweets:
+#     res = re.sub(r"@\w+", "<MENTIONS>", t)
+#     res = re.sub(r"#\w+", "<HASHTAGS>", res)
+#     # res_emoticon = re.sub(r"","EMOTICON",res_hashtags)
+#     # res_emojis = re.sub(r"","EMOJIS",res_hashtags)
+#     list = res.split()
+#     print(list)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
--- untitled1.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Nov 10 03:57:06 2022
-
-@author: chichungcheung
-"""
-
