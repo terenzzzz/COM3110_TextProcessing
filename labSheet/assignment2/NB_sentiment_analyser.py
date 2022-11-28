085258610cecdb0f595ae2863d8a742d51cb77a3
terenzzzz <terence592592@gmail.com>
2022-11-28 10:31:54 +0000

    'sync'

--- labSheet/assignment2/NB_sentiment_analyser.py
+++ labSheet/assignment2/NB_sentiment_analyser.py
@@ -6,12 +6,14 @@ Start code.
 """
 import argparse
 import pandas as pd
+import pickle
 
 from nltk.corpus import stopwords
 """
 IMPORTANT, modify this part with your details
 """
 USER_ID = "acc20zj" #your unique student ID, i.e. the IDs starting with "acp", "mm" etc that you use to login into MUSE 
+
 
 def parse_args():
     parser=argparse.ArgumentParser(description="A Naive Bayes Sentiment Analyser for the Rotten Tomatoes Movie Reviews dataset")
@@ -25,6 +27,161 @@ def parse_args():
     args=parser.parse_args()
     return args
 
+# read Phrases from file
+class Phrasesor:
+    def __init__(self,phraseId,sentent,sentiment):
+        self.phraseId = phraseId
+        self.sentent = sentent
+        self.sentiment = sentiment
+    
+    def phrases(filename):
+        df = pd.read_csv(filename,index_col=0, delimiter="\t")
+        phrases=[]
+        for index,row in df.iterrows():
+            phrase = Phrasesor(index,row['Phrase'].split(),row['Sentiment'])
+            phrases.append(phrase)
+        return phrases
+    
+
+# Preprosessor
+class Processor:
+    
+    def __init__(self,phrases):
+        self.phrases = phrases
+    
+    def preProsess(self):
+        for phrase in self.phrases:
+            newSentent=[]
+            for word in phrase.sentent:
+                # lowerCase
+                word = word.lower()
+                # StopList
+                if word not in stopwords.words('english'):
+                    newSentent.append(word)
+            phrase.sentent = newSentent
+        return self.phrases
+    
+    
+    def map5to3(self):
+        for phrase in self.phrases:
+            sentiment = phrase.sentiment
+            if sentiment == 0 or sentiment == 1:
+                phrase.sentiment = 0
+            elif sentiment == 2:
+                phrase.sentiment = 1
+            elif sentiment == 3 or sentiment == 4:
+                phrase.sentiment = 2
+        return self.phrases
+    
+# Trining     
+class Trainer:
+    def __init__(self, phrases):
+        self.phrases = phrases
+        self.classCountor()
+        self.phraseClassifier()
+        self.priorProbabilityCalc()
+        self.fearureCountor()
+        self.likelihoodCalc()
+        
+    def classCountor(self):
+        self.negCount=0
+        self.neuCount=0
+        self.posCount=0
+        for phrase in self.phrases:
+            sentiment = phrase.sentiment
+            if sentiment == 0:
+                self.negCount+=1
+            elif sentiment == 1:
+                self.neuCount+=1
+            elif sentiment == 2:
+                self.posCount+=1
+            else:
+                print("classCountor Error!")
+        self.classCount = self.negCount + self.neuCount + self.posCount
+        
+        
+    def phraseClassifier(self):
+        self.posPhrase = []
+        self.neuPhrase = []
+        self.negPhrase = []
+        for phrase in self.phrases:
+            sentiment = phrase.sentiment
+            if sentiment == 0:
+                self.posPhrase.append(phrase)
+            elif sentiment == 1:
+                self.neuPhrase.append(phrase)
+            elif sentiment == 2:
+                self.negPhrase.append(phrase)
+            else:
+                print("phraseClassifier Error!")
+                
+    def fearureCountor(self):
+        self.posFeatureCount = 0
+        self.posFeature = {}
+        for phrase in self.posPhrase:
+            for word in phrase.sentent:
+                if word in self.posFeature:
+                    self.posFeature[word]+=1
+                    self.posFeatureCount+=1
+                else:
+                    self.posFeature[word]=1
+                    self.posFeatureCount+=1
+                    
+        self.neuFeatureCount = 0
+        self.neuFeature = {}
+        for phrase in self.neuPhrase:
+            for word in phrase.sentent:
+                if word in self.neuFeature:
+                    self.neuFeature[word]+=1
+                    self.neuFeatureCount+=1
+                else:
+                    self.neuFeature[word]=1
+                    self.neuFeatureCount+=1
+                    
+        
+        self.negFeatureCount = 0
+        self.negFeature = {}
+        for phrase in self.negPhrase:
+            for word in phrase.sentent:
+                if word in self.negFeature:
+                    self.negFeature[word]+=1
+                    self.negFeatureCount+=1
+                else:
+                    self.negFeature[word]=1
+                    self.negFeatureCount+=1
+
+    def priorProbabilityCalc(self):
+        self.negPrior = self.negCount / self.classCount
+        self.neuPrior = self.neuCount / self.classCount
+        self.posPrior = self.posCount / self.classCount
+
+    
+    def likelihoodCalc(self):
+       self.posLikelihood = {}
+       for feature in self.posFeature:
+           self.posLikelihood[feature] = self.posFeature[feature] / self.posFeatureCount
+           
+       self.neuLikelihood = {}
+       for feature in self.neuFeature:
+            self.neuLikelihood[feature] = self.neuFeature[feature] / self.neuFeatureCount
+            
+       self.negLikelihood = {}
+       for feature in self.negFeature:
+            self.negLikelihood[feature] = self.negFeature[feature] / self.negFeatureCount
+    
+
+      
+# Pridicting    
+class Predictor:
+    def __init__(self, metadata):
+        self.metadata = metadata
+        
+    def predict(self,phrases):
+        result = []
+        for phrase in phrases:
+            print(phrase.sentent)
+    
+    
 
 def main():
     
@@ -60,188 +217,39 @@ def main():
     s∗ = argmax p(si) ∏ p(tj|si)
     """
     
+
     
-    # read Phrases from file
-    class Phrasesor:
-        def __init__(self,phraseId,sentent,sentiment):
-            self.phraseId = phraseId
-            self.sentent = sentent
-            self.sentiment = sentiment
-        
-        def phrases(filename):
-            df = pd.read_csv(dev,index_col=0, delimiter="\t")
-            phrases=[]
-            for index,row in df.iterrows():
-                phrase = Phrasesor(index,row['Phrase'].split(),row['Sentiment'])
-                phrases.append(phrase)
-            return phrases
-        
     
-    # Preprosessor
-    class Processor:
-        
-        def __init__(self,phrases):
-            self.phrases = phrases
-        
-        def preProsess(self):
-            for phrase in self.phrases:
-                newSentent=[]
-                for word in phrase.sentent:
-                    # lowerCase
-                    word = word.lower()
-                    # StopList
-                    if word not in stopwords.words('english'):
-                        newSentent.append(word)
-                phrase.sentent = newSentent
-            return self.phrases
-        
-        
-        def map5to3(self):
-            for phrase in self.phrases:
-                sentiment = phrase.sentiment
-                if sentiment == 0 or sentiment == 1:
-                    phrase.sentiment = 0
-                elif sentiment == 2:
-                    phrase.sentiment = 1
-                elif sentiment == 3 or sentiment == 4:
-                    phrase.sentiment = 2
-            return self.phrases
+################################## Training ######################################
+    #Preprocessing
+    # print('='*50,' Training','='*50)
+    # phrases = Phrasesor.phrases(training)
+    # processed = Processor(phrases).preProsess()
+    # to3Class = Processor(processed).map5to3()
+    # # Training
+    # trainer = Trainer(to3Class)
+    # with open('Training_model', 'wb') as f:
+    #         pickle.dump(trainer, f)
+
+    # print()
+    # print('='*50,'Trainning Result','='*50)
+    # print("Negative Prior Probability: ",trainer.posPrior)
+    # print("Neutral Prior Probability: ",trainer.neuPrior)
+    # print("Positive Prior Probability: ",trainer.posPrior)
     
-    class BayesClassifer:
-        
-        def __init__(self, phrases):
-            self.phrases = phrases
-            self.classCountor()
-            self.phraseClassifier()
-            self.fearureCountor()
-            self.likelihoodCalc()
-            
-            
-            
-        def classCountor(self):
-            self.negCount=0
-            self.neuCount=0
-            self.posCount=0
-            for phrase in self.phrases:
-                sentiment = phrase.sentiment
-                if sentiment == 0:
-                    self.negCount+=1
-                elif sentiment == 1:
-                    self.neuCount+=1
-                elif sentiment == 2:
-                    self.posCount+=1
-                else:
-                    print("classCountor Error!")
-            self.classCount = self.negCount + self.neuCount + self.posCount
-            
-            
-        def phraseClassifier(self):
-            self.posPhrase = []
-            self.neuPhrase = []
-            self.negPhrase = []
-            for phrase in self.phrases:
-                sentiment = phrase.sentiment
-                if sentiment == 0:
-                    self.posPhrase.append(phrase)
-                elif sentiment == 1:
-                    self.neuPhrase.append(phrase)
-                elif sentiment == 2:
-                    self.negPhrase.append(phrase)
-                else:
-                    print("phraseClassifier Error!")
-                    
-        def fearureCountor(self):
-            self.posFeatureCount = 0
-            self.posFeature = {}
-            for phrase in self.posPhrase:
-                for word in phrase.sentent:
-                    if word in self.posFeature:
-                        self.posFeature[word]+=1
-                        self.posFeatureCount+=1
-                    else:
-                        self.posFeature[word]=1
-                        self.posFeatureCount+=1
-                        
-            self.neuFeatureCount = 0
-            self.neuFeature = {}
-            for phrase in self.neuPhrase:
-                for word in phrase.sentent:
-                    if word in self.neuFeature:
-                        self.neuFeature[word]+=1
-                        self.neuFeatureCount+=1
-                    else:
-                        self.neuFeature[word]=1
-                        self.neuFeatureCount+=1
-                        
-            
-            self.negFeatureCount = 0
-            self.negFeature = {}
-            for phrase in self.negPhrase:
-                for word in phrase.sentent:
-                    if word in self.negFeature:
-                        self.negFeature[word]+=1
-                        self.negFeatureCount+=1
-                    else:
-                        self.negFeature[word]=1
-                        self.negFeatureCount+=1
-
-                
-            
-        
-        def priorProbabilityCalc(self):
-            self.negPrior = self.negCount / self.classCount
-            self.neuPrior = self.neuCount / self.classCount
-            self.posPrior = self.posCount / self.classCount
-            return self.negPrior,self.neuPrior,self.posPrior
-        
-        def likelihoodCalc(self):
-           self.posLikelihood = {}
-           for feature in self.posFeature:
-               self.posLikelihood[feature] = self.posFeature[feature] / self.posFeatureCount
-               
-           self.neuLikelihood = {}
-           for feature in self.neuFeature:
-                self.neuLikelihood[feature] = self.neuFeature[feature] / self.neuFeatureCount
-                
-           self.negLikelihood = {}
-           for feature in self.negFeature:
-                self.negLikelihood[feature] = self.negFeature[feature] / self.negFeatureCount
-        
-        def posteriorCalc(self):
-            for phrase in self.phrases:
-                print(phrase.phraseId)
-          
-            
-
-
-            
+################################## Developing ######################################
+    # load model
+    with open('Training_model', 'rb') as f:
+        corpus_meta = pickle.load(f)
+    predictor = Predictor(corpus_meta)
     
-
-
-
-    phrases = Phrasesor.phrases(training)
+    #Preprocessing
+    phrases = Phrasesor.phrases(dev)
     processed = Processor(phrases).preProsess()
     to3Class = Processor(processed).map5to3()
-
+    predictResult = predictor.predict(to3Class)
     
-    bayes = BayesClassifer(to3Class)
-    negPrior,neuPrior,posPrior = bayes.priorProbabilityCalc()
     
-    bayes.posteriorCalc()
-
-    
-
-    
-        
-          
-            
-            
-    
-    print()
-    print('='*50,'Prior Probability','='*50)
-    print("Negative Prior Probability: ",negPrior)
-    print("Neutral Prior Probability: ",neuPrior)
-    print("Positive Prior Probability: ",posPrior)
     #You need to change this in order to return your macro-F1 score for the dev set
     f1_score = 0
     
